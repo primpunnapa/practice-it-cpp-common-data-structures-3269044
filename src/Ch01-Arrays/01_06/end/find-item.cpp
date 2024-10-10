5aec6128f45f4aef61483683915af6f1b36ea61e
Punnapa <146909293+primpunnapa@users.noreply.github.com>
2024-10-10 06:41:08 +0000

    find item finish

--- src/Ch01-Arrays/01_06/end/find-item.cpp
+++ src/Ch01-Arrays/01_06/end/find-item.cpp
@@ -5,10 +5,36 @@ using namespace std;
 template <typename T>
 int findItem(const T arr[], size_t size, T target)
 {
-    // TODO: implement
+    int mid = size / 2;
+    if (arr[mid] == target)
+    {
+        return mid;
+    }
+    for (size_t i = 0; i < size; i++)
+    {
+        if (arr[i] == target)
+        {
+            return i;
+        }
+        else
+        {
+            return -1;
+        }
+    }
 }
 
 int main()
 {
+    string test[] = {"momo", "mim", "maria"};
+    string tem = "masha";
+    size_t testsize = sizeof(test)/sizeof(test[0]);
+    size_t result = findItem(test, testsize, tem);
+
+    if (result == -1){
+        cout << "item not found";
+    }
+    else{
+        cout << "found at " << result << "\n";
+    }
     return 0;
 }
